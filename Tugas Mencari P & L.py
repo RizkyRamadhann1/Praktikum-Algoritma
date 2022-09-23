2ef68f6bb733e2569161959d6c3c1e52a40cd212
Rizky Ramadhan <114083830+RizkyRamadhann1@users.noreply.github.com>
2022-09-23 10:07:22 +0700

    Add files via upload

---
+++ Tugas Mencari P & L.py
@@ -0,0 +1,6 @@
+print("Menghitung Luas Ruangan")
+p = float(input("Masukan panjang ruangan :"))
+l = float(input("Masukan lebar ruangan :"))
+s = str(input("Masukan satuan yang digunakan Meter/Inci? :"))
+Luas = p*l
+print("Maka Luas ruangan adalah: ",(Luas),(s))
