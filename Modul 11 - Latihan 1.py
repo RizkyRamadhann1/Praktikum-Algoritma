914c0f28465c4dc73df8004dc4d89560d875df2f
Rizky Ramadhan <114083830+RizkyRamadhann1@users.noreply.github.com>
2022-12-05 13:01:56 +0700

    Add files via upload

---
+++ Modul 11 - Latihan 1.py
@@ -0,0 +1,22 @@
+class Student:
+    'Common base class for all employees'
+    stdCount = 1
+    
+    def __init__(self, Name, NIM, Angkatan):
+        self.Name = Name
+        self.NIM = NIM
+        self.Angkatan = Angkatan
+        Student.stdCount = +1
+        
+    def displayStudent(self):
+        print ("Name", self.Name, "NIM", self.NIM, "Angkatan", self.Angkatan)
+    
+Name = input("Masukkan Nama:")
+NIM = input("Masukkan NIM:")
+Angkatan = input("Angkatan Tahun:")
+
+std=Student(Name ,NIM , Angkatan)
+std.displayStudent()
+print("Total Student %d" % Student.stdCount) 
+        
+    
